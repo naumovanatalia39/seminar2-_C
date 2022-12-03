725e314cb8abf2b35a3dadb4853ba12878adc3ad
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 19:57:13 +0200

    add task12

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert. ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a % b;
+// if (res > 0 || res < 0)
+// {
+//     Console.WriteLine($"Не кратно, остаток {res}");
+// }
+// else
+// {
+//     Console.WriteLine("Кратно");
+// }
+if (res == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {res}");
+}
