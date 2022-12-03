a49f6d7aa4bc8aa5189b1aba50598fb1314ba00d
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 19:16:00 +0200

    add task9

---
+++ task9/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(10, 100);
+int digit2 = number % 10;
+int digit1 = number / 10;
+Console.WriteLine($"Случайное число = {number}, первая цифра = {digit1}, вторая цифра = {digit2}");
+int max = digit1;
+if (max < digit2)
+{
+    max = digit2;
+}
+Console.WriteLine(max);
