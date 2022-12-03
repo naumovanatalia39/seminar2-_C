a4feb69e6d78ba2decf4fe799534b5bd688b7a4d
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 21:49:00 +0200

    add hometask13

---
+++ hometask13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a / 100 > 0)
+{
+    while (a / 1000 > 0)
+    {
+        a = a / 10;
+    }
+    Console.WriteLine(a % 10);
+}
+else
+{
+    Console.WriteLine("Нет третьего числа");
+}
