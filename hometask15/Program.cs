b51abcb7e0d5de4214fcf08f9f6585078136adaf
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 21:59:57 +0200

    add hometask15

---
+++ hometask15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    if (a > 0 && a < 8)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Цифра не соответствует дню недели");
+    }
+}
