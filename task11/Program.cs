44275d701377c6284a56c25ebd8518a2ef71d91b
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 19:28:05 +0200

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random(). Next(100, 1000);
+int digit1 = number / 100;
+int digit3 = number % 10;
+int result = (digit1 * 10) + digit3;
+Console.WriteLine($"Случайное число = {number}"); 
+Console.WriteLine($"После удаления второй цифры этого числа получается {result}");
