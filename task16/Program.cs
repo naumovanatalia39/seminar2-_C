353c215efc0d3c78b1680012306c6ceecaa4b762
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 20:27:42 +0200

    add task16

---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert. ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert. ToInt32(Console.ReadLine());
+if (a == b * b || b == a * a)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
