337c5cc6a0dedf5799d995428022f71c85137169
naumovanatalia <naumova.na43@mail.ru>
2022-12-03 21:01:27 +0200

    add hometask10

---
+++ hometask10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a / 100 > 0 )
+{
+    Console.WriteLine((a - (a / 100) * 100) / 10);
+
+}
+else
+{
+    Console.WriteLine("Число не подходит");
+}
